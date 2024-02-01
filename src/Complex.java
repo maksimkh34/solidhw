be93d823858ae35acdc39c82109675d2a3eb9f65
maksimkh <maksimkh34@duck.com>
2024-02-01 23:48:28 +0300

    undone

--- src/Complex.java
+++ src/Complex.java
@@ -1,2 +1,46 @@
 public class Complex {
+    public int a = 0, b = 1;
+
+    Complex(int a_, int b_) {
+        a = a_;
+        b = b_;
+    }
+
+    Complex() {
+        this(0, 1);
+    }
+
+    Complex(String str) {
+        getFromString(str);
+    }
+
+    public String formatToStr() {
+        String result = "";
+        if(a != 0){
+            result = String.valueOf(a);
+        }
+
+        if(b >= 0 && a != 0) result += "+";
+        result += String.valueOf(b);
+        result += "i";
+
+        return result;
+    }
+
+    public void getFromString(String str) {
+        String[] nums = new String[] {"", ""};
+        if(str.contains("+")) {
+            nums = str.split("\\+");
+        } else if (str.contains("-")) {
+            nums = str.split("-");
+        } else {
+            a = 0;
+            b = Integer.parseInt(str.substring(str.length() - 1));
+            return;
+        }
+
+        nums[1] = nums[1].substring(nums[1].length() - 1);
+        a = Integer.parseInt(nums[0]);   // Не работают отриц числа
+        b = Integer.parseInt(nums[1]);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Complex a1 = new Complex("5+4i");
+        Complex a2 = new Complex("-5-6i");
+        Complex a3 = new Complex("-4+5i");
+        Complex a4 = new Complex("7-1i");
+        System.out.printf("%s\n%s\n%s\n%s\n%n",
+                a1.formatToStr(),
+                a2.formatToStr(),
+                a3.formatToStr(),
+                a4.formatToStr());
     }
 }
