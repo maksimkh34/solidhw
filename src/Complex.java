c339268e06de74d057f3a2e7b92f3047b1f475e6
maksimkh <maksimkh34@duck.com>
2024-02-02 23:01:23 +0300

    Complex Class done (weak tests)

--- src/Complex.java
+++ src/Complex.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Complex {
     public int a = 0, b = 1;
 
@@ -10,11 +12,15 @@ public class Complex {
         this(0, 1);
     }
 
-    Complex(String str) {
+    Complex(String str) throws Exception {
         getFromString(str);
     }
 
     public String formatToStr() {
+        if(b==0) {
+            return String.valueOf(a);
+        }
+
         String result = "";
         if(a != 0){
             result = String.valueOf(a);
@@ -27,20 +33,34 @@ public class Complex {
         return result;
     }
 
-    public void getFromString(String str) {
-        String[] nums = new String[] {"", ""};
-        if(str.contains("+")) {
-            nums = str.split("\\+");
-        } else if (str.contains("-")) {
-            nums = str.split("-");
-        } else {
-            a = 0;
-            b = Integer.parseInt(str.substring(str.length() - 1));
+    public void getFromString(String str) throws Exception {
+        if(!str.contains("i")) {
+            this.a = Integer.parseInt(str);
+            this.b = 0;
             return;
         }
 
-        nums[1] = nums[1].substring(nums[1].length() - 1);
-        a = Integer.parseInt(nums[0]);   // Не работают отриц числа
-        b = Integer.parseInt(nums[1]);
+        if(str.matches("-?[0-9]+i")) {
+            this.a = 0;
+            this.b = Integer.parseInt(str.substring(0, str.length()-1));
+            return;
+        }
+
+        str = str.substring(0, str.length()-1);
+        Integer[] nums = new Integer[] {0, 0};
+        for(int i = 2; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i)) && (str.charAt(i-1) == '-' || str.charAt(i-1) == '+')) {
+                    nums[0] = Integer.parseInt(str.substring(0, i - 1));
+                    nums[1] = Integer.parseInt(str.substring(i - 1));
+                };
+
+        }
+
+        if(Arrays.equals(nums, new Integer[]{0, 0})) {
+            throw new Exception("This is not complex number. ");
+        }
+
+        this.a = nums[0];
+        this.b = nums[1];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Complex a1 = new Complex("5+4i");
-        Complex a2 = new Complex("-5-6i");
-        Complex a3 = new Complex("-4+5i");
-        Complex a4 = new Complex("7-1i");
+        Complex a1, a2, a3, a4;
+        try {
+            a1 = new Complex("4i");
+            a2 = new Complex("-5");
+            a3 = new Complex("-4+5i");
+            a4 = new Complex("-1i");
+        } catch (Exception e) {
+            System.out.println("There is not complex number! ");
+            return;
+        }
         System.out.printf("%s\n%s\n%s\n%s\n%n",
                 a1.formatToStr(),
                 a2.formatToStr(),
